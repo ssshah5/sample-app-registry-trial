08db882e912b5197f6656104958c5954f55156a1
Shrenik Shah <ssshah@us.ibm.com>
2017-02-12 01:01:12 -0500

    Added Dockerfile

--- app.js
+++ app.js
@@ -8,9 +8,9 @@
 // for more info, see: http://expressjs.com
 var express = require('express');
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+
+// Constants
+var PORT = 8080;
 
 // create a new express server
 var app = express();
@@ -18,12 +18,6 @@ var app = express();
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-
-	// print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
